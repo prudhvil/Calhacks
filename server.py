f4b5a4adc0131de2caf73dd7c34b3d9399a42172
Prudhvi Lokireddy <prudhvil@berkeley.edu>
2015-10-10 07:45:39 -0700

    added a test route

--- server.py
+++ server.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
 	return 'Hello world'
+@app.route('/test')
+def test():
+	return jsonify(**{'status': 1,'result': ['val1','val2']})
 
 if __name__ == '__main__':
 	app.run(debug=True)
