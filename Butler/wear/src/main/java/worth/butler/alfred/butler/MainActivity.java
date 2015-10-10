2e9aafb23186ded5ab0e1f1738036fdf462fd8c2
Prudhvi Lokireddy <prudhvil@berkeley.edu>
2015-10-10 09:12:48 -0700

    added button for voice input

--- Butler/wear/src/main/java/worth/butler/alfred/butler/MainActivity.java
+++ Butler/wear/src/main/java/worth/butler/alfred/butler/MainActivity.java
@@ -27,12 +27,13 @@ import java.util.List;
 public class MainActivity extends Activity {
 
     private SpeechRecognizer sr;
-    private static final String TAG = "MyStt3Activity";
+    private static final String TAG = ">Wear MainActivity<";
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
         sr = SpeechRecognizer.createSpeechRecognizer(this);
         sr.setRecognitionListener(new listener());
 
@@ -42,6 +43,35 @@ public class MainActivity extends Activity {
 
         intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS,5);
         sr.startListening(intent);
+        */
+    }
+
+    private static final int SPEECH_REQUEST_CODE = 0;
+
+    // Create an intent that can start the Speech Recognizer activity
+    private void displaySpeechRecognizer(View view) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+// Start the activity, the intent will be populated with the speech text
+        startActivityForResult(intent, SPEECH_REQUEST_CODE);
+    }
+
+    // This callback is invoked when the Speech Recognizer returns.
+// This is where you process the intent and extract the speech text from the intent.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = results.get(0);
+            // Do something with spokenText
+            Intent sendMessageIntent = new Intent(MainActivity.this,SendMessageService.class);
+            sendMessageIntent.putExtra("command",spokenText);
+            startService(sendMessageIntent);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     class listener implements RecognitionListener
