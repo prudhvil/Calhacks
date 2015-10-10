7256b912e02c6a72617ba6c06c47a7d65eaa51c5
Prudhvi Lokireddy <prudhvil@berkeley.edu>
2015-10-10 10:25:12 -0700

    added post request frame

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
-from flask import Flask,jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+commands = ['light on', ]
 
 @app.route('/')
 def hello():
@@ -8,6 +10,11 @@ def hello():
 @app.route('/test')
 def test():
 	return jsonify(**{'status': 1,'result': ['val1','val2']})
+
+@app.route('/command',methods=['POST'])
+def do_command():
+	command = request.form['command']
+	#do some shit with command
 
 if __name__ == '__main__':
 	app.run(debug=True)
